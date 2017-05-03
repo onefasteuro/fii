3d812f4728bd171c54cadc210cef483c43204d73
Oliver <onefasteuro@gmail.com>
2017-05-03 18:53:38 -0400

    finished facility session code

--- src/Course.php
+++ src/Course.php
@@ -13,20 +13,4 @@ Class Course extends \Illuminate\Database\Eloquent\Model {
         $key = md5(self::COURSES_CACHE_KEY);
         return $key;
     }
-
-    public static function getFiiCourse($id)
-    {
-        return static::where('fii_course_id','=', $id)->first();
-    }
-
-
-    public static function getCourses()
-    {
-        $key = static::getCacheKey();
-        $response = remember($key, CACHE_WEEK, function(){
-            return static::orderBy('start_date', 'DESC')->get();
-        });
-        return $response;
-
-    }
 }
---
+++ src/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FII;
+
+
+class ServiceProvider extends \WPBootstrapper\Providers\AbstractServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        $this->container->singleton('fii', function($container){
+            $config = $container->make('config')->get('fii');
+            $cache = $container->make('cache');
+            $provider = new Provider($config, $cache);
+            return $provider;
+        });
+    }
+}
